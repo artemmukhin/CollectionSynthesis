68203d32b85f845f00e912e8387835ed15d43fb3
ortem <ortem00@gmail.com>
2017-12-16 15:33:03 +0300

    Start new term generator

--- src/test.hs
+++ src/test.hs
@@ -1,113 +1,80 @@
 module Main where
-
-data FuncTtoInt_ = FuncTtoInt_ deriving(Show)
-data FuncTtoBool_ = FuncTtoBool_ deriving(Show)
-data FuncTtoT_ = FuncTtoT_ deriving(Show)
-data List_ = List_ deriving(Show)
-data T_ = T_ deriving (Show)
+import Data.Map(Map, fromList, lookup)
+import Data.Maybe(fromMaybe)
 
 
-data Expr
-  = IntExpr
-  | ListExpr
-  | TExpr
-  | MyListExpr
-  | MySetExpr
-  | MyMapExpr
-  deriving(Show)
+data PrimType
+  = Int
+  | Bool
+  | Poly String
+  deriving (Show, Eq)
 
-data ListExpr
-  = MyList_content MyListExpr
-  | MySet_content MySetExpr
-  deriving(Show)
+data TypeConstructor
+  = MyList
+  | MyMap
+  | MySet
+  deriving (Show, Eq)
 
-data TExpr
-  = MyList_head MyListExpr
-  | MyList_last MyListExpr
-  | MyList_maximize FuncTtoInt_ MyListExpr
-  | MyList_find FuncTtoBool_ MyListExpr
+data Type
+  = Prim PrimType
+  | List PrimType
+  | Func [Type] Type
+  | Constructor TypeConstructor [Type]
+  deriving (Show, Eq)
 
-  | MySet_find T_ MySetExpr
+data Identifier = Identifier String deriving (Show)
+data Function   = Function { name :: String, domain :: [Type], range :: Type }
 
-  | MyMap_get T_ MyMapExpr
-  deriving(Show)
+instance Show Function where
+  show = name
 
-data MyListExpr
-  = MyList_build List_
-  | MyList_tail MyListExpr
-  | MyList_map FuncTtoT_ MyListExpr
-  | MyList_filter FuncTtoBool_ MyListExpr
-  | MyList_concat MyListExpr MyListExpr
-  deriving(Show)
+data Term
+  = Hole PrimType
+  | HoleList PrimType
+  | Var Identifier Type
+  | Abstraction [Identifier] Term
+  | Application Function [Term]
+  deriving (Show)
 
-data MySetExpr
-  = MySet_build List_
-  | MySet_add T_ MySetExpr
-  | MySet_filter FuncTtoBool_ MySetExpr
-  | MySet_union MySetExpr MySetExpr
-  deriving(Show)
+functions :: [Function]
+functions = [Function { name    = "List.build",
+                        domain  = [List (Poly "t")],
+                        range   = Constructor MyList [Prim (Poly "t")] },
+             Function { name    = "List.map",
+                        domain   = [Func [Prim (Poly "t")] (Prim (Poly "t")), Constructor MyList [Prim (Poly "t")]],
+                        range    = Constructor MyList [Prim (Poly "t")] }
+            ]
 
-data MyMapExpr
-  = MyMap_build List_
-  | MyMap_add T_ T_ MyMapExpr
-  deriving(Show)
+functionsMap :: Data.Map.Map String Function
+functionsMap = Data.Map.fromList $ zip (map name functions) functions
 
-data IntExpr
-  = MyList_size MyListExpr
-  | MySet_size MySetExpr
-  | MyMap_size MyMapExpr
-  deriving(Show)
+getFunction :: String -> Function
+getFunction funcname = fromMaybe (error "No such function") f
+  where f = Data.Map.lookup funcname functionsMap
 
-class ExprGenerator coll where
-  funcBuild             :: coll
-  funcsCollToColl       :: [coll -> coll]
-  funcsColl2ToColl      :: [coll -> coll -> coll]
-  funcsCollToList       :: [coll -> ListExpr]
-  funcsCollToInt        :: [coll -> IntExpr]
-  funcsCollToT          :: [coll -> TExpr]
+functionsByRange :: Type -> [Function]
+functionsByRange r = filter (\f -> range f == r) functions
 
-  generate :: [coll] -> [coll]
-  generate []       = generate [funcBuild]
-  generate current  = current ++ generate next where
-      next = concatMap addUnary current ++ concatMap addBinary (cartProd current current)
-      cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+-- todo: type unification
+functionsByTypeConstr :: TypeConstructor -> [Type] -> [Function]
+functionsByTypeConstr constr types =
+  filter (\f -> case range f of
+    Constructor c _ -> c == constr
+    _ -> False) functions
 
-  addUnary :: coll -> [coll]
-  addUnary expr = map (\f -> f expr) funcsCollToColl
+freshvar :: [Term] -> Type -> Identifier
+freshvar env _ = Identifier ("a" ++ show i) where
+  i = length $ filter (\t -> case t of Var _ _ -> True; _ -> False) env
 
-  addBinary :: (coll, coll) -> [coll]
-  addBinary (e1,e2) = concatMap (\f -> [f e1 e2, f e2 e1]) funcsColl2ToColl
-
-
-instance ExprGenerator MyListExpr where
-  funcBuild        = MyList_build List_
-  funcsCollToColl  = [MyList_tail, MyList_map FuncTtoT_, MyList_filter FuncTtoBool_]
-  funcsColl2ToColl = [MyList_concat]
-  funcsCollToList  = [MyList_content]
-  funcsCollToInt   = [MyList_size]
-  funcsCollToT     = [MyList_head, MyList_last, MyList_maximize FuncTtoInt_, MyList_find FuncTtoBool_]
-
-instance ExprGenerator MySetExpr where
-  funcBuild        = MySet_build List_
-  funcsCollToColl  = [MySet_add T_, MySet_filter FuncTtoBool_]
-  funcsColl2ToColl = [MySet_union]
-  funcsCollToList  = [MySet_content]
-  funcsCollToInt   = [MySet_size]
-  funcsCollToT     = [MySet_find T_]
-
-instance ExprGenerator MyMapExpr where
-  funcBuild        = MyMap_build List_
-  funcsCollToColl  = [MyMap_add T_ T_]
-  funcsColl2ToColl = []
-  funcsCollToList  = []
-  funcsCollToInt   = [MyMap_size]
-  funcsCollToT     = [MyMap_get T_]
-
-
-myListExprs = generate [] :: [MyListExpr]
-listExprs   = [f e | e <- myListExprs, f <- funcsCollToColl]
-tExprs      = [f e | e <- myListExprs, f <- funcsCollToT]
-intExprs    = [f e | e <- myListExprs, f <- funcsCollToInt]
+generate :: [Term] -> Type -> [Term]
+generate _ (Prim t) = [Hole t]
+generate _ (List t) = [HoleList t]
+generate env (Func dom ran) = [Abstraction vars body | body <- bodies] where
+  vars = map (freshvar env) dom
+  bodies = generate (env ++ zipWith Var vars dom) ran
+generate env (Constructor constr types) =
+  [Application f args | f <- functionsByTypeConstr constr types,
+                        args <- sequence [generate env t | t <- domain f]]
 
 
 main :: IO ()
