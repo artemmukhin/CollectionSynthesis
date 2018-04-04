7d0773b22d73920d2c71579fbcd9ae49ecf70d5f
ortem <ortem00@gmail.com>
2018-04-04 18:35:02 +0300

    Add show; fix Expr

--- src/CoreLanguage.hs
+++ src/CoreLanguage.hs
@@ -1,39 +1,62 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 module CoreLanguage where
 
 data Type
   = Int
+  | Bool
   | List Type
   deriving (Eq)
 
 instance Show Type where
   show Int      = "Int"
+  show Bool     = "Bool"
   show (List t) = "[" ++ show t ++ "]"
 
-newtype Id = Id String
-instance Show Id where show (Id s) = s
+
+newtype Param = Param (String, Type)
+instance Show Param where
+  show (Param (s, _)) = s
+
 
 data Func
-  = Map     Type Type FuncTerm
-  | Filter  Type Type FuncTerm
+  = Map { dom :: Param, codom :: Type, body :: Expr }
+  | Filter { dom :: Param, body :: Expr }
 
-data FuncTerm
+instance Show Func where
+  show (Map d _ b) = "map (λ" ++ show d ++ ". " ++ show b ++ ")"
+  show (Filter d b) = "filter (λ" ++ show d ++ ". " ++ show b ++ ")"
+
+
+data PrimExpr
   = Var String
-  -- Int
   | IntConst Integer
-  | Plus FuncTerm FuncTerm
-  | Mul FuncTerm FuncTerm
-  -- List
-  | Length FuncTerm
-  | Concat FuncTerm FuncTerm
-  | Head FuncTerm
+  | Unary String PrimExpr
+  | Binary String PrimExpr PrimExpr
+
+instance Show PrimExpr where
+  show (Var s) = s
+  show (IntConst n) = show n
+  show (Unary op e) = op ++ " " ++ show e
+  show (Binary op e1 e2)  = show e1 ++ " " ++ op ++ " " ++ show e2
+
 
 data Expr
   = Hole Type
-  | Collection Id Type
+  | Collection String Type
   | App Func Expr
+  | Prim PrimExpr
+
+instance Show Expr where
+  show (Hole t) = "_" ++ show t ++ "_"
+  show (Collection name _) = name
+  show (App f expr@(App _ _)) = show f ++ " (" ++ show expr ++ ")"
+  show (App f expr) = show f ++ " " ++ show expr
+  show (Prim expr) =  show expr
+
 
 typeOf :: Expr -> Type
 typeOf (Hole t) = t
 typeOf (Collection _ t)  = t
-typeOf (App (Map _ b _) _) = b
-typeOf (App (Filter _ b _) _) = b
+typeOf (App (Map _ b _) _) = List b
+typeOf (App (Filter _ _) _) = Bool
+typeOf (Prim _) = undefined
--- src/CoreTest.hs
+++ src/CoreTest.hs
@@ -1,15 +1,37 @@
 module Test where
 import CoreLanguage
 
-funcTerm :: FuncTerm
-funcTerm = Plus (Head (Var "xs")) (IntConst 42)
+funcTerm :: Expr
+funcTerm = Prim (Binary "+" (Unary "head" (Var "xs")) (IntConst 42))
 
 func :: Func
-func = Map (List Int) Int funcTerm
+func = Map{ dom = Param ("xs", List Int), codom = Int, body = funcTerm }
 
 expr1 :: Expr
-expr1 = Collection (Id "xss") (List (List Int))
+expr1 = Collection "xss" (List (List Int))
 
 expr2 :: Expr
 expr2 = App func expr1
--- expr ~ map (\xs -> head xs + 42) xss
+-- expr2 ~ map (\xs -> head xs + 42) xss
+
+expr3 :: Expr
+expr3 = App Map{ dom = Param ("x", Int), codom = Int, body = fterm } e_xs where
+  e_xs = Collection "xs" (List Int)
+  e_ys = Collection "ys" (List Int)
+  fterm = App Filter{ dom = Param ("y", Int), body = Prim (Binary "==" (Binary "%" (Var "x") (Var "y")) (IntConst 0)) } e_ys
+-- expr3 = map (\x. filter (\y. x % y == 0) ys) xs
+
+
+testTypes :: Bool
+testTypes =
+  typeOf expr1 == List (List Int) &&
+  typeOf expr2 == List Int &&
+  typeOf expr3 == List Int
+
+testShow :: Bool
+testShow =
+  show expr2 == "map (λxs. head xs + 42) xss" &&
+  show expr3 == "map (λx. filter (λy. x % y == 0) ys) xs"
+
+test :: Bool
+test = testTypes && testShow
