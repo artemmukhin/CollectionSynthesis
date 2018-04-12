d55907606ca73d0d0a214efbd2b7d015be81d48a
ortem <ortem00@gmail.com>
2018-04-12 15:57:21 +0300

    Add reduce

--- src/CoreLanguage.hs
+++ src/CoreLanguage.hs
@@ -13,7 +13,7 @@ instance Show Type where
   show (List t) = "[" ++ show t ++ "]"
 
 
-newtype Param = Param (String, Type)
+newtype Param = Param (String, Type) deriving (Eq)
 instance Show Param where
   show (Param (s, _)) = s
 
@@ -21,7 +21,8 @@ instance Show Param where
 data Func
   = Map { dom :: Param, codom :: Type, body :: Expr }
   | Filter { dom :: Param, body :: Expr }
-  | MapFilter { dom :: Param, codom :: Type, bodyMap :: Expr, bodyFilter :: Expr }
+  | MapFilter { dom :: Param, codom :: Type, bodyFilter :: Expr, bodyMap :: Expr }
+  deriving (Eq)
 
 showLambda :: Param -> Expr -> String
 showLambda d b = "(λ" ++ show d ++ ". " ++ show b ++ ")"
@@ -29,13 +30,15 @@ showLambda d b = "(λ" ++ show d ++ ". " ++ show b ++ ")"
 instance Show Func where
   show (Map d _ b) = "map " ++ showLambda d b
   show (Filter d b) = "filter " ++ showLambda d b
-  show (MapFilter d _ bm bf) = "mapFilter " ++ showLambda d bm ++ " " ++ showLambda d bf
+  show (MapFilter d _ bf bm) = "mapFilter " ++ showLambda d bf ++ " " ++ showLambda d bm
+
 
 data PrimExpr
   = Var String
   | IntConst Integer
   | Unary String PrimExpr
   | Binary String PrimExpr PrimExpr
+  deriving (Eq)
 
 instance Show PrimExpr where
   show (Var s) = s
@@ -49,6 +52,8 @@ data Expr
   | Collection String Type
   | App Func Expr
   | Prim PrimExpr
+  | Compose Expr Expr
+  deriving (Eq)
 
 instance Show Expr where
   show (Hole t) = "_" ++ show t ++ "_"
@@ -56,6 +61,8 @@ instance Show Expr where
   show (App f expr@(App _ _)) = show f ++ " (" ++ show expr ++ ")"
   show (App f expr) = show f ++ " " ++ show expr
   show (Prim expr) =  show expr
+  show (Compose e1 e2) = "((" ++ show e1 ++ ") . (" ++ show e2 ++ "))"
+
 
 typeOf :: Expr -> Type
 typeOf (Hole t) = t
@@ -64,3 +71,38 @@ typeOf (App (Map _ cod _) _) = List cod
 typeOf (App (Filter _ _) _) = Bool
 typeOf (App (MapFilter _ cod _ _) _) = List cod
 typeOf (Prim _) = undefined
+typeOf (Compose e _) = typeOf e
+
+{-
+filter p (map f xs)           =  mapFilter (p . f) f xs  =  map (\x -> if p (f x) then f x else None)
+map f (filter p xs)           =  mapFilter p     f xs  =  map (\x -> if p x then f x else None)
+filter p' (mapFilter p f xs)  =  mapFilter (\x -> p x && p' x) f xs
+map f' (mapFilter p f xs)     =  mapFilter p (f' . f) xs
+mapFilter p f (filter p' xs)  =  mapFilter (\x -> p x && p' x) f xs
+mapFilter p f (map f' xs)     =  mapFilter (p . f') (f . f') xs
+-}
+
+data ReduceState = Complete | Incomplete deriving (Eq, Show)
+
+reduceStep :: Expr -> (ReduceState, Expr)
+reduceStep e@(Hole _) = (Complete, e)
+reduceStep e@(Collection _ _) = (Complete, e)
+reduceStep e@(Prim _) = (Complete, e)
+reduceStep (App (Map _ c f) (App (Map d _ f') xs)) = (Incomplete, App (Map d c (Compose f f')) xs)
+reduceStep (App (Map _ c f) (App (Filter d p) xs)) = (Incomplete, App (MapFilter d c p f) xs)
+reduceStep (App (Map _ c f) (App (MapFilter d _ p f') xs)) = (Incomplete, App (MapFilter d c p (Compose f f')) xs)
+reduceStep e@(App Map{} _) = (Complete, e)
+reduceStep (App (Filter _ p) (App (Filter d p') xs)) = (Incomplete, App (Filter d (Compose p p')) xs)
+reduceStep (App (Filter _ p) (App (Map d c f) xs)) = (Incomplete, App (MapFilter d c (Compose p f) f) xs)
+reduceStep (App (Filter _ p) (App (MapFilter d c p' f) xs)) = (Incomplete, App (MapFilter d c (Compose p p') f) xs)
+reduceStep e@(App Filter{} _) = (Complete, e)
+reduceStep (App (MapFilter _ c p f) (App (MapFilter d' _ p' f') xs)) = (Incomplete, App (MapFilter d' c (Compose p' (Compose p f')) (Compose f f')) xs)
+reduceStep (App (MapFilter _ c p f) (App (Filter d' p') xs)) = (Incomplete, App (MapFilter d' c (Compose p p') f) xs)
+reduceStep (App (MapFilter _ c p f) (App (Map d' _ f') xs)) = (Incomplete, App (MapFilter d' c (Compose p f') (Compose f f')) xs)
+reduceStep e@(App MapFilter{} _) = (Complete, e)
+reduceStep e@(Compose _ _) = (Complete, e)
+
+reduce :: Expr -> Expr
+reduce expr = snd $ go (Incomplete, expr) where
+  go (Incomplete, e) = go (reduceStep e)
+  go (Complete, e)   = (Complete, e)
--- src/CoreTest.hs
+++ src/CoreTest.hs
@@ -1,46 +1,80 @@
 module Test where
 import CoreLanguage
 
-funcTerm :: Expr
-funcTerm = Prim (Binary "+" (Unary "head" (Var "xs")) (IntConst 42))
-
-func :: Func
-func = Map{ dom = Param ("xs", List Int), codom = Int, body = funcTerm }
+xsExpr :: Expr
+xsExpr = Collection "xs" (List Int)
+ysExpr :: Expr
+ysExpr = Collection "ys" (List Int)
+xssExpr :: Expr
+xssExpr = Collection "xss" (List (List Int))
 
 expr1 :: Expr
-expr1 = Collection "xss" (List (List Int))
+expr1 = Prim (Binary "+" (Unary "head" (Var "xs")) (IntConst 42))
+
+func :: Func
+func = Map{ dom = Param ("xs", List Int), codom = Int, body = expr1 }
+
+f :: Expr
+f = Prim (Binary "+" (Var "x") (IntConst 1))
+p :: Expr
+p = Prim (Binary "==" (Binary "%" (Var "x") (IntConst 2)) (IntConst 0))
+p' :: Expr
+p' = Prim (Binary "<" (Binary "*" (Var "x") (IntConst 3)) (IntConst 10))
+
 
 expr2 :: Expr
-expr2 = App func expr1
+expr2 = App func xssExpr
 -- expr2 ~ map (\xs -> head xs + 42) xss
 
 expr3 :: Expr
-expr3 = App Map{ dom = Param ("x", Int), codom = Int, body = fterm } e_xs where
-  e_xs = Collection "xs" (List Int)
-  e_ys = Collection "ys" (List Int)
-  fterm = App Filter{ dom = Param ("y", Int), body = Prim (Binary "==" (Binary "%" (Var "x") (Var "y")) (IntConst 0)) } e_ys
+expr3 = App Map{ dom = Param ("x", Int), codom = Int, body = fterm } xsExpr where
+  fterm = App Filter{ dom = Param ("y", Int), body = Prim (Binary "==" (Binary "%" (Var "x") (Var "y")) (IntConst 0)) } ysExpr
 -- expr3 = map (\x. filter (\y. x % y == 0) ys) xs
 
 expr4 :: Expr
-expr4 = App MapFilter{ dom = Param ("x", Int), codom = Int, bodyMap = mbody, bodyFilter = fbody } e_xs where
-  e_xs = Collection "xs" (List Int)
-  mbody = Prim (Binary "+" (Var "x") (IntConst 1))
-  fbody = Prim (Binary "==" (Binary "%" (Var "x") (IntConst 2)) (IntConst 0))
+expr4 = App MapFilter{ dom = Param ("x", Int), codom = Int, bodyFilter = p, bodyMap = f } xsExpr
 -- expr4 = mapFilter (\x. x + 1) (\x. x % 2 == 0) xs
 
+expr5 :: Expr
+expr5 = App Map{ dom = Param ("x", Int), codom = Int, body = f } e_filter where
+  e_filter = App Filter{ dom = Param ("x", Int), body = p } xsExpr
+-- expr5 = map (λx. x + 1) (filter (λx. x % 2 == 0) xs)
+
+expr6 :: Expr
+expr6 = App Map{ dom = Param ("x", Int), codom = Int, body = f } e_filter where
+  e_filter = App Filter{ dom = Param ("x", Int), body = p } e_filter'
+  e_filter' = App Filter{ dom = Param ("x", Int), body = p' } xsExpr
+-- expr6 = map (λx. x + 1) (filter (λx. x % 2 == 0) (filter (λx. x * 3 < 10) xs))
+
+expr7 :: Expr
+expr7 = App MapFilter{ dom = Param ("x", Int), codom = Int, bodyFilter = Compose p p', bodyMap = f } xsExpr
+-- expr7 = mapFilter (λx. ((x % 2 == 0) . (x * 3 < 10))) (λx. x + 1) xs
 
 
 testTypes :: Bool
 testTypes =
-  typeOf expr1 == List (List Int) &&
+  typeOf xssExpr == List (List Int) &&
   typeOf expr2 == List Int &&
-  typeOf expr3 == List Int
+  typeOf expr3 == List Int &&
+  typeOf expr4 == List Int &&
+  typeOf expr5 == List Int
 
 testShow :: Bool
 testShow =
   show expr2 == "map (λxs. head xs + 42) xss" &&
   show expr3 == "map (λx. filter (λy. x % y == 0) ys) xs" &&
-  show expr4 == "mapFilter (λx. x + 1) (λx. x % 2 == 0) xs"
+  show expr4 == "mapFilter (λx. x % 2 == 0) (λx. x + 1) xs" &&
+  show expr5 == "map (λx. x + 1) (filter (λx. x % 2 == 0) xs)" &&
+  show expr6 == "map (λx. x + 1) (filter (λx. x % 2 == 0) (filter (λx. x * 3 < 10) xs))"
+
+testReduce :: Bool
+testReduce =
+  reduce expr1 == expr1 &&
+  reduce expr2 == expr2 &&
+  reduce expr3 == expr3 &&
+  reduce expr4 == expr4 &&
+  reduce expr5 == expr4 &&
+  reduce expr6 == expr7
 
 test :: Bool
-test = testTypes && testShow
+test = testTypes && testShow && testReduce
